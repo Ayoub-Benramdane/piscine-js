f6de314787f3bb9c8c001a046bbffdc62d1391bb
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2024-10-17 20:32:58 +0100

    new

---
+++ deep-copy.js
@@ -0,0 +1,14 @@
+
+function deepCopy(origine) {
+    if (origine === null || typeof origine !== 'object' || origine instanceof RegExp) {
+        return origine
+    }
+    if (Array.isArray(origine)) return origine.map(item => deepCopy(item))
+    const copiedObject = {}
+    for (const key in origine) {
+        if (origine.hasOwnProperty(key)) {
+            copiedObject[key] = deepCopy(origine[key])
+        }
+    }
+    return copiedObject
+}
